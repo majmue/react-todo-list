d6cebd1b844eddb6a62aae05505896a271efd08c
majmue <post.maja.mueller@gmail.com>
2018-11-11 23:15:58 +0100

    Commit after npm build
    Signed-off-by: majmue <post.maja.mueller@gmail.com>

---
+++ docs/precache-manifest.f094ba0210acbbb36178bc369f1aa24b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dd41535c598384ce3ddc",
+    "url": "/static/js/main.dd41535c.chunk.js"
+  },
+  {
+    "revision": "fa92568d88c7bfad485c",
+    "url": "/static/js/1.fa92568d.chunk.js"
+  },
+  {
+    "revision": "dd41535c598384ce3ddc",
+    "url": "/static/css/main.156e28f4.chunk.css"
+  },
+  {
+    "revision": "95b9c1b37a04e9da0e632a273d1d2e4b",
+    "url": "/index.html"
+  }
+];
